86bdac1611a6c0ecf1138789a3c76eba2445fa90
Musty23456 <Mustaphasalisu093@gmail.com>
2026-09-23 23:26:54 +0100

    Update messageRoutes.ts

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -3,7 +3,7 @@ import * as messageController from '../controllers/messageController';
 import * as mediaController from '../controllers/mediaController';
 import { authenticate } from '../middleware/authenticate';
 import { validate } from '../middleware/validate';
-import { createMessageSchema } from '../utils/schemas';
+import { createMessageSchema, editMessageSchema, forwardMessageSchema, reactMessageSchema } from '../utils/schemas';
 import { upload } from '../middleware/upload';
 import { asyncHandler } from '../utils/asyncHandler';
 
@@ -13,10 +13,10 @@ router.use(authenticate);
 router.get('/:conversationId', asyncHandler(messageController.listMessages));
 router.post('/', validate(createMessageSchema), asyncHandler(messageController.sendMessage));
 router.post('/upload', upload.single('file'), asyncHandler(mediaController.uploadAttachment));
-router.patch('/:id', asyncHandler(messageController.editMessage));
+router.patch('/:id', validate(editMessageSchema), asyncHandler(messageController.editMessage));
 router.delete('/:id', asyncHandler(messageController.deleteMessage));
 router.post('/:id/star', asyncHandler(messageController.toggleStar));
-router.post('/:id/react', asyncHandler(messageController.reactToMessage));
-router.post('/:id/forward', asyncHandler(messageController.forwardMessage));
+router.post('/:id/react', validate(reactMessageSchema), asyncHandler(messageController.reactToMessage));
+router.post('/:id/forward', validate(forwardMessageSchema), asyncHandler(messageController.forwardMessage));
 
 export default router;
